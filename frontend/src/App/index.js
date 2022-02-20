3c6554f847c7b5b1993079715a380daf970edc3a
Santiago Feijoo <santiago.feijoo96@gmail.com>
2022-02-20 16:12:05 -0300

    pequeño posible refactor

--- frontend/src/App/index.js
+++ frontend/src/App/index.js
@@ -1,55 +1,20 @@
 import Navbar from '../components/Navbar'
 import { useAppContext } from '../context/appContext';
 
-import Home from './screens/Home'
-import Admin from './screens/Admin'
-import NotFound from './screens/NotFound'
-
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
   Outlet,
   useNavigate
 } from "react-router-dom";
+
 import { useEffect } from 'react'
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route indexed path="/" element={
-              <Home />
-            }
-            />
-            <Route
-              path="/admin"
-              element={
-                <Admin />
-              }
-            />
-          </Route>
-          <Route
-            path="*"
-            element={
-              <NotFound />
-            }
-          />
-        </Routes>
-      </Router>
-    </>
-  );
-}
-
-const Layout = () => {
-  const { account, contract, setLoading } = useAppContext()
+  const { account, contract } = useAppContext()
   const navigate = useNavigate()
 
   const checkAdmin = async () => {
     const _owner = await contract.owner()
-    if (_owner.toLowerCase() == account.toLowerCase()) {
+    if (_owner.toLowerCase() === account.toLowerCase()) {
       navigate("/admin")
     }
   }
@@ -67,7 +32,7 @@ const Layout = () => {
       <Navbar />
       <Outlet />
     </>
-  )
+  );
 }
 
 export default App;
